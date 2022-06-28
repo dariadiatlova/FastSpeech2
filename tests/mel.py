4f5b794e7b359f772032496f82bb779b09d9e86b
dariadiatlova <dari.diatlova@gmail.com>
2022-06-28 15:13:37 +0300

    debug

--- tests/mel.py
+++ tests/mel.py
@@ -17,20 +17,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
-    compute_mel_energy = ComputeMelEnergy(config.sample_rate, config.filter_length, config.hop_length,
-                                          config.win_length, config.n_mels, config.device)
+    compute_mel_energy = ComputeMelEnergy(config["sample_rate"], config["filter_length"], config["hop_length"],
+                                          config["win_length"], config["n_mels"], config["device"])
 
     for filename in config.test_filenames:
         # get mel online
         basename = filename.split(".")[0]
-        tg_path = os.path.join(config.source_wav_directory, "{}.TextGrid".format(filename))
-        wav_path = os.path.join(config.source_wav_directory, "{}.wav".format(filename))
+        tg_path = os.path.join(config["source_wav_directory"], "{}.TextGrid".format(filename))
+        wav_path = os.path.join(config["source_wav_directory"], "{}.wav".format(filename))
         textgrid = tgt.io.read_textgrid(tg_path)
-        preprocessor = Preprocessor(config.preprocess_config_path)
+        preprocessor = Preprocessor(config["preprocess_config_path"])
         _, _, start, end = preprocessor.get_alignment(textgrid.get_tier_by_name("phones"))
-        wav = librosa.load(wav_path, sr=config.sample_rate)[0]
-        wav = wav[int(config.sample_rate * start):int(config.sampe_rate * end)].astype(np.float32)
+        wav = librosa.load(wav_path, sr=config["sample_rate"])[0]
+        wav = wav[int(config["sample_rate"] * start):int(config["sample_rate"] * end)].astype(np.float32)
         online_mel, energy = get_mel_from_wav(wav, compute_mel_energy)
         # load mel from saved
-        saved_mel = np.load(os.path.join(config.source_wav_directory, "0-duration-{}.npy".format(filename)))
+        saved_mel = np.load(os.path.join(config["source_wav_directory"], "0-duration-{}.npy".format(filename)))
         assert np.allclose(online_mel, saved_mel), "Mel Spectrograms aren't closed due to the set tolerance"
