70c9563102370d26ceaccdf8ece49e5fed8fa9b3
dariadiatlova <dari.diatlova@gmail.com>
2022-08-07 16:54:21 +0300

    added vad from the end

--- datasets/vctk/vad_and_resample.py
+++ datasets/vctk/vad_and_resample.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import torch
 import torchaudio
 import torchaudio.transforms as T
 import torchaudio.functional as F
@@ -9,8 +10,9 @@ from tqdm import tqdm
 def main(dir_path: str, sample_rate: int = 48000, resample_rate: int = 16000):
     speakers = os.listdir(f"{dir_path}/wav48_silence_trimmed")
     vad = T.Vad(sample_rate)
-    new_dir_path = f"{dir_path}/wavs16vad"
+    new_dir_path = f"{dir_path}/wavs16vad2"
     os.makedirs(new_dir_path, exist_ok=True)
+    counter = 0
     for speaker in tqdm(speakers):
         prev_dir_path = f"{dir_path}/wav48_silence_trimmed/{speaker}"
         if os.path.isdir(prev_dir_path):
@@ -20,7 +22,7 @@ def main(dir_path: str, sample_rate: int = 48000, resample_rate: int = 16000):
                 if "mic2" in audio_filepath:
                     continue
                 wav, sr = torchaudio.load(audio_filepath)
-                vad_wav = vad(wav)
+                vad_wav = torch.flip(vad(torch.flip(vad(wav), dims=[1])), dims=[1])
                 resampled = F.resample(vad_wav, sample_rate, resample_rate)
                 short_filename = filename[:8]
                 txt_filepath = f"{dir_path}/txt/{speaker}/{short_filename}.txt"
@@ -28,7 +30,8 @@ def main(dir_path: str, sample_rate: int = 48000, resample_rate: int = 16000):
                     shutil.copy(txt_filepath, f"{new_dir_path}/{short_filename}.txt")
                     torchaudio.save(f"{new_dir_path}/{short_filename}.wav", resampled, sample_rate=resample_rate)
                 else:
-                    print(f"{txt_filepath} not found :c\n")
+                    counter += 1
+    print(f"Resampled and added wavs are saved to the {new_dir_path}!\n Couldn't find txt for {counter} samples :c")
 
 
 if __name__ == "__main__":
