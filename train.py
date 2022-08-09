10a9646e338cad4c3784c58323f192341c9af64e
dariadiatlova <dari.diatlova@gmail.com>
2022-08-09 12:33:53 +0300

    upd params for new training

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def train(cfg) -> None:
 
     train_loader = get_dataloader(config=cfg.preprocess, train=True)
     synthesis_loader = get_dataloader(config=cfg.preprocess, train=False)
-    assert len(synthesis_loader) == 2, f"Length of val loader: {synthesis_loader}, " \
+    assert len(synthesis_loader) <= 2, f"Length of val loader: {synthesis_loader}, " \
                                        f"only first 2 batches will be synthesized."
     model = FastSpeechLightning(cfg)
     if cfg.checkpoint_path is not None:
